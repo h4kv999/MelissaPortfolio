b33512562ae589bc26dcf8020ff5d26f0a04608a
H4KV <vitor.sannctorum@hotmail.com>
2024-02-25 22:15:28 -0300

    Test: Flip cards mobile v2

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,36 +6,36 @@ export function Cards(){
     return(
         <Fragment>
             <Headline text="Your destiny"/>
-            <div className="flex gap-4 border border-purple-100 p-4 rounded-md">
-                <div className="group h-[500px] w-96 perspective-1000">
+            <div className="flex justify-center gap-4 border border-purple-100 px-4 py-8 rounded-md">
+                <div className="group h-[482px] w-[270px] perspective-1000">
                     <div className="relative h-full w-full rounded-xl transition-all duration-500 transform-style-3d group-hover:[transform:rotateY(180deg)]">
-                        <div className="absolute inset-0">
-                            <img src={ images.cards.card1 } alt="" className="w-full h-fit" />
+                        <div className="absolute inset-0 h-full">
+                            <img src={ images.cards.card1 } alt="" className="w-full h-full object-cover" />
                         </div>
                         <div className="absolute inset-0 h-full w-full text-center [transform:rotateY(180deg)] backface-hidden ">
-                            <img src={ images.cards.card2 } className="w-full h-fit" alt="" />
-                        </div>
-                    </div>
-                </div>
-                
-                <div className="group h-[500px] w-96 perspective-1000">
-                    <div className="relative h-full w-full rounded-xl transition-all duration-500 transform-style-3d group-hover:[transform:rotateY(180deg)]">
-                        <div className="absolute inset-0">
-                            <img src={ images.cards.card1 } alt="" className="w-full h-fit" />
-                        </div>
-                        <div className="absolute inset-0 h-full w-full text-center [transform:rotateY(180deg)] backface-hidden ">
-                            <img src={ images.cards.card2 } className="w-full h-fit" alt="" />
+                            <img src={ images.cards.card2 } className="w-full h-full object-cover" alt="" />
                         </div>
                     </div>
                 </div>
 
-                <div className="group h-[500px] w-96 perspective-1000">
+                <div className="group h-[482px] w-[270px] perspective-1000">
                     <div className="relative h-full w-full rounded-xl transition-all duration-500 transform-style-3d group-hover:[transform:rotateY(180deg)]">
-                        <div className="absolute inset-0">
-                            <img src={ images.cards.card1 } alt="" className="w-full h-fit" />
+                        <div className="absolute inset-0 h-full">
+                            <img src={ images.cards.card1 } alt="" className="w-full h-full object-cover" />
                         </div>
                         <div className="absolute inset-0 h-full w-full text-center [transform:rotateY(180deg)] backface-hidden ">
-                            <img src={ images.cards.card2 } className="w-full h-fit" alt="" />
+                            <img src={ images.cards.card2 } className="w-full h-full object-cover" alt="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="group h-[482px] w-[270px] perspective-1000">
+                    <div className="relative h-full w-full rounded-xl transition-all duration-500 transform-style-3d group-hover:[transform:rotateY(180deg)]">
+                        <div className="absolute inset-0 h-full">
+                            <img src={ images.cards.card1 } alt="" className="w-full h-full object-cover" />
+                        </div>
+                        <div className="absolute inset-0 h-full w-full text-center [transform:rotateY(180deg)] backface-hidden ">
+                            <img src={ images.cards.card2 } className="w-full h-full object-cover" alt="" />
                         </div>
                     </div>
                 </div>
